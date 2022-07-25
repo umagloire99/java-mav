6b5483531e734d2b9cd4b8f40cfce544b243a10c
umagloire <umagloire99@gmail.com>
2022-07-25 13:32:19 +0100

    fixing the docker push
    Signed-off-by: umagloire <umagloire99@gmail.com>

--- script.groovy
+++ script.groovy
@@ -8,7 +8,7 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t umagloire/demo-app:2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'echo docker push umagloire/demo-app:2.0'
+        sh 'docker push umagloire/demo-app:2.0'
     }
 }
 
