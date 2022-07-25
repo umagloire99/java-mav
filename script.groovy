2a0589f9adcb0a7819815de5e60524be28b14f47
umagloire <umagloire99@gmail.com>
2022-07-25 13:25:32 +0100

    fixing the build image function
    Signed-off-by: umagloire <umagloire99@gmail.com>

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def buildImage() {
     echo 'building docker Image and push to docker hub ......'
-    withCredentials(usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t umagloire/demo-app:2.0 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'echo docker push umagloire/demo-app:2.0'
